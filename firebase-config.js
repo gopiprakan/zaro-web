1eb142dc92149ed5785d32a432a88050f62ab653
Gopiprakan <gopiprakan2006@gmail.com>
2026-06-10 23:00:17 +0530

    feat: configure Firebase environment variables and update authentication settings

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // ZARO Firebase Configuration
-window.FIREBASE_API_KEY = "your_firebase_api_key_here";
-window.FIREBASE_AUTH_DOMAIN = "your_firebase_auth_domain_here";
-window.FIREBASE_PROJECT_ID = "your_firebase_project_id_here";
-window.FIREBASE_STORAGE_BUCKET = "your_firebase_storage_bucket_here";
-window.FIREBASE_MESSAGING_SENDER_ID = "your_firebase_messaging_sender_id_here";
-window.FIREBASE_APP_ID = "your_firebase_app_id_here";
+window.FIREBASE_API_KEY = "AIzaSyBefJKwkR1gWbvb2U6vvKu1iykhmapkDio";
+window.FIREBASE_AUTH_DOMAIN = "zaro-4756c.firebaseapp.com";
+window.FIREBASE_PROJECT_ID = "zaro-4756c";
+window.FIREBASE_STORAGE_BUCKET = "zaro-4756c.firebasestorage.app";
+window.FIREBASE_MESSAGING_SENDER_ID = "965541921452";
+window.FIREBASE_APP_ID = "1:965541921452:web:32b5c2faf6af2f7d575509";
